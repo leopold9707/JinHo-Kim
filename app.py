8e5c4d904d387630828426077b318e8d154acced
leopold9707 <59679768+leopold9707@users.noreply.github.com>
2020-03-16 03:53:09 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+    return jsonify({"text" : "Hello, world!"})
+
+@app.route('/iam/<name>')
+def whoami(name):
+    return jsonify({"Your name" : name})
+
+@app.route('/login', methods = ['POST'])
+def login():
+    uid = request.get_json()
+    return jsonify(uid)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080")
